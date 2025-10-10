534936fcbbab9ee49f12b205e4c368b684e5927d
Navil Talukdar <naviltalukdar3@gmail.com>
2025-10-10 03:32:17 -0400

    added rest of photos in assets

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <div className='text-center md:text-6xl text-6xl lg:text-8xl m-25' style={{ fontFamily: 'Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif' }}>
       <p>
-        WELCOME! I'M GOLAM MORTUZA, <span className='text-neutral-400'>AND I TAKE PHOTOS AND VIDEOS TO BRING </span> YOUR <span className='text-yellow-500'>VISION</span> TO LIFE
+        WELCOME! I'M GOLAM MORTUZA <span className='text-neutral-400'>AND I TAKE PHOTOS AND VIDEOS TO BRING </span> YOUR <span className='text-yellow-500'>VISION</span> TO LIFE
       </p>
     </div>
   )
