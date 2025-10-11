eb339370072d01f355810414e6084e490aa3c2ff
Navil Talukdar <naviltalukdar3@gmail.com>
2025-10-11 16:26:51 -0400

    added mobile responsivenesss with hamburger menu for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/Logo.png'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div className='flex justify-between w-full h-25 px-5 border-b-1 border-b-neutral-700'>
-      <div>
-        <Link to={'/'}><img src={logo} className='object-contain h-full w-full'/></Link>
+    <div className='w-full border-b-1 border-b-neutral-700 bg-black'>
+      <div className='flex justify-between h-25 px-5'>
+        <div>
+          <Link to={'/'}><img src={logo} className='object-contain h-full w-full'/></Link>
+        </div>
+        <ul className='hidden md:flex text-xl text-white font-extrabold gap-x-8 items-center pr-5'>
+          <Link to={'../Cars'}><li className='hover:text-yellow-500'>CARS</li></Link>
+          <Link to={'../Landscapes'}><li className='hover:text-yellow-500'>LANDSCAPES</li></Link>
+          <li className='hover:text-yellow-500'><a target='_blank' href='https://www.instagram.com/golamsvision/'>IG</a></li>
+          <li className='hover:text-yellow-500'><a target='_blank' href='https://www.tiktok.com/@golamsvision'>TIKTOK</a></li>
+        </ul>
+        <button className='md:hidden text-white' onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          ) : (
+            <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          )}
+        </button>
       </div>
-      <ul className='text-xl text-shadow-neutral-50 font-extrabold flex gap-x-8 justify-center items-center pr-5'>
-        <Link to={'../Cars'}><li className='hover:text-yellow-500'>CARS</li></Link>
-        <Link to={'../Landscapes'}><li className='hover:text-yellow-500'>LANDSCAPES</li></Link>
-        <li className='hover:text-yellow-500'><a target='_blank' href='https://www.instagram.com/golamsvision/'>IG</a></li>
-        <li className='hover:text-yellow-500'><a target='_blank' href='https://www.tiktok.com/@golamsvision'>TIKTOK</a></li>
-      </ul>
+      {isOpen && (
+        <div className='md:hidden bg-black border-t border-neutral-700'>
+          <ul className='flex flex-col text-xl text-white font-extrabold py-4'>
+            <Link to={'../Cars'} onClick={() => setIsOpen(false)}><li className='hover:text-yellow-500 px-5 py-3'>CARS</li></Link>
+            <Link to={'../Landscapes'} onClick={() => setIsOpen(false)}><li className='hover:text-yellow-500 px-5 py-3'>LANDSCAPES</li></Link>
+            <li className='hover:text-yellow-500 px-5 py-3'><a target='_blank' href='https://www.instagram.com/golamsvision/' onClick={() => setIsOpen(false)}>IG</a></li>
+            <li className='hover:text-yellow-500 px-5 py-3'><a target='_blank' href='https://www.tiktok.com/@golamsvision' onClick={() => setIsOpen(false)}>TIKTOK</a></li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
