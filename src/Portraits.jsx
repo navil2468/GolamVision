b4137f1323fde762bc7351aad4b565383b561d5b
Navil Talukdar <naviltalukdar3@gmail.com>
2025-10-14 00:38:54 -0400

    portraits files added

---
+++ src/Portraits.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Portraits() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Portraits
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from './App.jsx'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Cars from './Cars.jsx';
 import Landscapes from './Landscapes.jsx';
+import Portraits from './Portraits.jsx';
 
 const router = createBrowserRouter([
   {path:'/', element: <App />},
   {path:'/Cars', element: <Cars />},
-  {path:'/Landscapes', element: <Landscapes />}
+  {path:'/Landscapes', element: <Landscapes />},
+  {path: '/Portraits', element: <Portraits />}
 ]
 )
 
