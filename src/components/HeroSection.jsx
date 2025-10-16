466fce8cb7f8cf4fae765905aedc738b8e0f7e1e
Navil Talukdar <naviltalukdar3@gmail.com>
2025-10-16 01:58:04 -0400

    minor herosection tweak

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,10 +7,15 @@ function HeroSection() {
       <p className='md:text-6xl text-6xl lg:text-8xl' style={{ fontFamily: 'Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif' }}>
         WELCOME! I'M GOLAM MORTUZA <span className='text-neutral-400 '>AND I TAKE PHOTOS AND VIDEOS TO BRING YOUR</span> <span className='text-yellow-500'>VISION</span> TO LIFE
       </p>
-      <div className='flex justify-center gap-8 my-15'>
+      <div className='flex flex-wrap justify-center gap-8 my-15'>
         <Link to={'/Cars'}>
           <h1 className='border-2 border-white px-8 py-4 text-2xl font-bold hover:border-yellow-500 hover:text-yellow-500 transition-colors'>
             CARS
+          </h1>
+        </Link>
+        <Link to={'/Portraits'}>
+          <h1 className='border-2 border-white px-8 py-4 text-2xl font-bold hover:border-yellow-500 hover:text-yellow-500 transition-colors'>
+            PORTRAITS
           </h1>
         </Link>
         <Link to={'/Landscapes'}>
